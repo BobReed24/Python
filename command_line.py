8d0bff04bc9cff6b2cef55770ef632aef8211fea
Hunkon <153524469+BobReed24@users.noreply.github.com>
2025-01-24 09:37:22 -0600

    Update command_line.py

--- command_line.py
+++ command_line.py
@@ -1,15 +1,21 @@
 import os
-def main():
+def start():
     os.system('cls||clear')
-    while True:
-        try:
-            cur_dir = os.getcwd()
-            res = input(f"{cur_dir} (python) % ")
-            if "cd" in res:
-                res = res.replace("cd ", "")
-                os.chdir(res)
-            else:
-                os.system(res)
-        except (SyntaxError, NameError, TypeError, ValueError) as e:
-            print(f"Err: {e}")
-main()
+    main()
+def main():
+    try:
+        while True:
+            try:
+                cur_dir = os.getcwd()
+                res = input(f"{cur_dir} (python) % ")
+                if "cd" in res:
+                    res = res.replace("cd ", "")
+                    os.chdir(res)
+                else:
+                    os.system(res)
+            except (SyntaxError, NameError, TypeError, ValueError) as e:
+                print(f"Err: {e}")
+    except KeyboardInterrupt:
+        print("")
+        main()
+start()
